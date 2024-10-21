391f12ed43928582f5e1f0a6782e5af8a37c1afc
Jai Patel <jaipatel4717@gmail.com>
2024-10-21 10:22:21 -0500

    Added the add node function

--- CS201R-Unit10a/LinkedList.cpp
+++ CS201R-Unit10a/LinkedList.cpp
@@ -10,17 +10,41 @@ LinkedList::LinkedList(dataType d) {
 	Node* tempPtr = new Node;
 	tempPtr->data = d;
 	tempPtr->nextPtr = nullptr;
+	headPtr = tempPtr;
+	tailPtr = tempPtr;
 }
 LinkedList::~LinkedList() {
+	
 	cout << "DESTRUCTOR\n";
 }
 void LinkedList::addNode(dataType d) {
+	//create a new node with data d
+	Node* tempPtr = new Node;
+	tempPtr->data = d;
+	tempPtr->nextPtr = nullptr;
+	//add the node to the end of the list
+	if (headPtr == nullptr) {
+		headPtr = tempPtr;
+		tailPtr = tempPtr;
+	}
+	else {
+		tailPtr->nextPtr = tempPtr;
+		tailPtr = tempPtr;
+	}
+	
 	cout << "need to implement addnode\n";
 }
 void LinkedList::addNodeOrdered(dataType d) {
 	cout << "need to implement addnode ordered\n";
 }
 void LinkedList::delNode(string name) {
+	//delete a node with the name
+	//if the node is not in the list, print an error message
+	//if the node is in the list, delete the node
+	//if the node is the head, update the head
+	//if the node is the tail, update the tail
+	//if the node is in the middle, update the pointers
+	
 	cout << "need to implement delnode\n";
 }
 void LinkedList::printList() {
